29116193eb9ef80d45a8e179ba7459a2d818f623
wiet32522 <devriesj241@gmail.com>
2026-06-24 21:46:49 +0200

    Update DivazScript.lua

--- DivazScript.lua
+++ DivazScript.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Script is down right now its back a few hours after the Divaz update gets released")
+game.Players.LocalPlayer:Kick("Script is down right now. its back a few hours after the Divaz update gets released.")
