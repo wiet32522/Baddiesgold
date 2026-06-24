d7c7d29c6cf6e90f0c193825af1b84acac3f103c
wiet32522 <devriesj241@gmail.com>
2026-06-24 21:42:14 +0200

    Update DivazScript.lua

--- DivazScript.lua
+++ DivazScript.lua
@@ -1,5 +1,1 @@
-local player = game.Players:FindFirstChild("PlayerName")
-
-if player then
-    player:Kick("Script is down right now its back a few hours after the Divaz update gets released")
-end
+game.Players.LocalPlayer:Kick("Script is down right now its back a few hours after the Divaz update gets released")
